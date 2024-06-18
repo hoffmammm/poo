fb4c53895801be3628e40d8e808a6838db37998b
EDUARDO_HOFFMAM_DE_OLIVEIRA_COSTA <12201022@SIGMAB09.BARROCA>
2024-06-18 08:14:07 -0300

    mensagem

---
+++ aula18.06/ControleDeVinil.Shared.Dados/DAL.cs
@@ -0,0 +1,19 @@
+﻿using ControleDeVinil.Shared.Modelos;
+
+namespace ControleDeVinil.Shared.Dados
+{
+    public class DAL
+    {
+        private readonly Contexto context;
+
+        public List<Artista> Listar()
+        {
+            var lista = new List<Artista>
+            {
+                new Artista {Id = 1, Nome = "Luiz Felipe", Bio = "Autor de livro infantil", FotoPerfil = ""}
+            };
+            return lista;
+
+        }
+    }
+}
---
+++ aula18.06/ControleDeVinil.Shared.Modelos/Artista.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleDeVinil.Shared.Modelos
+{
+    public class Artista
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Bio { get; set; }
+        public string FotoPerfil { get; set; }
+    }
+}
